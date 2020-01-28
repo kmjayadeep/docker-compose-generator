b6beb344228bed3e56019c989a4849a288b260b2
Jayadeep KM <kmjayadeep@gmail.com>
2020-01-28 15:03:19 +0530

    added logic to generate yml file
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,45 @@
 #!/usr/bin/env node
 
-const yargs = require("yargs");
+const yargs = require('yargs');
+const YAML = require('json2yaml');
+const fs = require('fs');
+
 
 const options = yargs
   .usage('Usage: dcg [options]\nGenerate a sample docker-compose.yml file based on options')
   .option('s', { alias: 'service', describe: 'Service name', type: 'string', default: 'web' })
   .option('i', { alias: 'image', describe: 'Docker image', type: 'string', default: 'nginx:latest' })
-  .option('p', { alias: 'port', describe: 'Port to expose', type: "string", default: '3000:80' })
+  .option('p', { alias: 'port', describe: 'Port to expose', type: 'string', default: '3000:80' })
+  .option('build', { describe: 'Add build option', type: 'boolean', default: false })
+  .option('w', { alias: 'write', describe: 'Write to file', type: 'boolean', default: false })
+  .option('f', { alias: 'file', describe: 'File name to write', type: 'string', default: 'docker-compose.yml' })
   .argv;
 
 
-console.log(options)
+const service = {
+  [options.service]: {
+    image: options.image,
+    ports: [
+      options.port
+    ]
+  }
+}
+
+if (options.build) {
+  service[options.service].build = '.';
+}
+
+const template = {
+  version: '3',
+  services: {
+    ...service
+  }
+};
+
+const ymlText = YAML.stringify(template);
+
+console.log(ymlText);
+
+if (options.write) {
+  fs.writeFileSync(options.file, ymlText);
+}
